596a38e870dd5c94ffe9c996ac12606c5c612f22
Nnenna Iheke <nniheke@gmail.com>
2023-03-29 12:18:10 -0400

    updated permissions to use extension func

--- app/src/main/java/com/example/digitalenvoyassessment/MainActivity.kt
+++ app/src/main/java/com/example/digitalenvoyassessment/MainActivity.kt
@@ -81,7 +81,7 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
-            1 -> {
+            PERMISSION_REQUEST_CODE -> {
                 if (grantResults.isNotEmpty()
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED
                 )
